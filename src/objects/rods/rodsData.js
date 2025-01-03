1f6a1fa961a365c928f60350ef43abd53d5d2ddd
Maj <zorko.maj@gmail.com>
2025-01-03 16:33:00 +0100

    Added menu, collection and shop
    Added menu with a start and tutorial button, a collection of caught fish and a shop.

---
+++ src/objects/rods/rodsData.js
@@ -0,0 +1,34 @@
+const rodsData = [
+    {
+        name: "Basic",
+        fishChance: 0.75,
+        modelPath: "",
+        price: 2000,
+        bought: false,
+    },
+    {
+        name: "Upgraded",
+        fishChance: 0.89,
+        modelPath: "",
+        price: 6000,
+        bought: false,
+    },
+    {
+        name: "Max",
+        fishChance: 0.99,
+        modelPath: "",
+        price: 10000,
+        bought: false,
+    },
+
+    // default rod
+    {
+        name: "Stick",
+        fishChance: 0.65,
+        modelPath: "",
+        price: 0,
+        bought: true,
+    },
+]
+
+export default rodsData;
